3c9b9d72c023413198b2dbe722f4da2c3fa97ed1
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-10 14:28:03 +0530

    Create main_navigation_screen.dart

---
+++ lib/screens/main_navigation_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import '../core/constants/app_colors.dart';
+import 'home_screen.dart';
+import 'walk_history_screen.dart';
+import 'menu_screen.dart';
+
+class MainNavigationScreen extends StatefulWidget {
+  const MainNavigationScreen({super.key});
+
+  @override
+  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
+}
+
+class _MainNavigationScreenState extends State<MainNavigationScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const WalkHistoryScreen(),
+    const MenuScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        selectedItemColor: AppColors.primary,
+        unselectedItemColor: Colors.grey,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            label: 'Walk History',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.menu),
+            label: 'Menu',
+          ),
+        ],
+      ),
+    );
+  }
+}
