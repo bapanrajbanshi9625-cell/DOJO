2df7cf7a6963f3ec06d0111453fd0565c784be4b
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-10 15:35:39 +0530

    Initialize Firebase in main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'core/constants/app_colors.dart';
 import 'screens/login_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const DojoWalkerApp());
 }
 
