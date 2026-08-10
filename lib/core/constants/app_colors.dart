c64f669380469ab92222c8240e8f8a265d392c13
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-10 14:25:22 +0530

    Add AppColors class with primary and secondary colors

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFFD35435);
+  static const Color secondary = Color(0xFF3B82F6);
+  static const Color background = Color(0xFFF8FAFC);
+}
