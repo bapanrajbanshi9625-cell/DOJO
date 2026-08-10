6521c917ce882dff3b165b49582b0320cc499657
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-10 18:05:54 +0530

    Fix widget test to reference 'Dojo' class
    Updated widget test to use the correct main widget class name 'Dojo'.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,11 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
-import 'package:dojo/main.dart'; // Apne app ka package name
+import 'package:dojo/main.dart'; // Aapke app ka package name
 
 void main() {
   testWidgets('Dojo App smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp()); // Agar aapke main widget ka naam MyApp hai
+    // Build our app and trigger a frame using the correct class name 'Dojo'
+    await tester.pumpWidget(const Dojo()); 
 
     expect(find.byType(MaterialApp), findsOneWidget);
   });
