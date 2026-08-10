140264858d4a1fdea310e8f7fbc06d5293f6bd57
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-10 15:32:10 +0530

    Update color constants for secondary, background, and text

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color primary = Color(0xFFD35435);
-  static const Color secondary = Color(0xFF3B82F6);
-  static const Color background = Color(0xFFF8FAFC);
+  static const Color secondary = Color(0xFF2C3E50);
+  static const Color background = Color(0xFFF8F9FA);
+  static const Color textPrimary = Color(0xFF333333);
 }
