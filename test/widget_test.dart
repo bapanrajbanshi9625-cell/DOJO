d76d67b9d195b829788fed5d2fac695e0e255e55
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-10 18:11:21 +0530

    Update widget test to import app.dart and use DojoApp

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,11 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
-import 'package:dojo/main.dart'; // Aapke app ka package name
+import 'package:dojo/app.dart'; // Yahan main.dart ki jagah app.dart import karna hoga
 
 void main() {
   testWidgets('Dojo App smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame using the correct class name 'Dojo'
-    await tester.pumpWidget(const Dojo()); 
+    // Build our app and trigger a frame using the correct class name 'DojoApp'
+    await tester.pumpWidget(const DojoApp()); 
 
     expect(find.byType(MaterialApp), findsOneWidget);
   });
