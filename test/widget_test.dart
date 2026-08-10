edd6a6aa6dff0bb4abe5055ff15009bad8eb13e5
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-10 17:55:39 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,13 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
-import 'package:dojo/main.dart'; // Yahan 'dojo' ki jagah apne project ka actual package name likh sakte hain
+import 'package:dojo/main.dart'; // Apne app ka package name
 
 void main() {
-  testWidgets('App smoke test', (WidgetTester tester) async {
+  testWidgets('Dojo App smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp()); // Agar aapke main widget ka naam alag hai toh use yahan badal lein
+    await tester.pumpWidget(const MyApp()); // Agar aapke main widget ka naam MyApp hai
 
-    // Verify that our app starts up without crashing.
     expect(find.byType(MaterialApp), findsOneWidget);
   });
 }
