ad73ced0fd9d495424bc9b99f30e15e09834e114
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-14 04:10:04 +0530

    Add ProfileSectionCard widget for profile setup

---
+++ lib/features/profile_setup/widgets/profile_section_card.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class ProfileSectionCard extends StatelessWidget {
+  final Widget child;
+
+  const ProfileSectionCard({
+    super.key,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius:
+            BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color:
+                Colors.black.withOpacity(0.05),
+            blurRadius: 12,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: child,
+    );
+  }
+}
