9386dbf56badda270a26c17fc4a254533ba393d5
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-14 11:30:03 +0530

    Update main_navigation_screen.dart

--- lib/screens/main_navigation_screen.dart
+++ lib/screens/main_navigation_screen.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
+
 import '../core/constants/app_colors.dart';
 import 'home_screen.dart';
-import 'walks_screen.dart';
 import 'menu_screen.dart';
+import '../features/walks/walks_screen.dart';
 
 class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
 
   @override
-  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
+  State<MainNavigationScreen> createState() =>
+      _MainNavigationScreenState();
 }
 
-class _MainNavigationScreenState extends State<MainNavigationScreen> {
+class _MainNavigationScreenState
+    extends State<MainNavigationScreen> {
   int _currentIndex = 0;
 
   final List<Widget> _screens = [
     const HomeScreen(),
-    const WalkHistoryScreen(),
+    const WalksScreen(),
     const MenuScreen(),
   ];
 
@@ -24,24 +27,35 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: _screens[_currentIndex],
+
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
-        selectedItemColor: AppColors.primary,
-        unselectedItemColor: Colors.grey,
+
+        selectedItemColor:
+            AppColors.primary,
+
+        unselectedItemColor:
+            Colors.grey,
+
         onTap: (index) {
           setState(() {
             _currentIndex = index;
           });
         },
+
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: 'Home',
           ),
+
           BottomNavigationBarItem(
-            icon: Icon(Icons.history),
+            icon: Icon(
+              Icons.directions_walk_rounded,
+            ),
             label: 'Walks',
           ),
+
           BottomNavigationBarItem(
             icon: Icon(Icons.menu),
             label: 'Menu',
