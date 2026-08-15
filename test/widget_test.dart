3640d3ea43dc45e2edffca702110b4fee2060d9e
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-15 16:49:21 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,12 +1,20 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
-import 'package:dojo/app.dart'; // Yahan main.dart ki jagah app.dart import karna hoga
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:dojo_walk/app.dart';
 
 void main() {
-  testWidgets('Dojo App smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame using the correct class name 'DojoApp'
-    await tester.pumpWidget(const DojoApp()); 
+  testWidgets(
+    'Dojo Walk app smoke test',
+    (WidgetTester tester) async {
+      await tester.pumpWidget(
+        const DojoApp(),
+      );
 
-    expect(find.byType(MaterialApp), findsOneWidget);
-  });
+      expect(
+        find.byType(MaterialApp),
+        findsOneWidget,
+      );
+    },
+  );
 }
