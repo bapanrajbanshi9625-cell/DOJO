d5f9f8cfc7f673e1b4b5831916883dcbf788e553
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-23 01:53:41 +0530

    Update qr_service.dart

--- lib/services/qr_service.dart
+++ lib/services/qr_service.dart
@@ -1,9 +1,10 @@
 import 'dart:async';
+import 'dart:convert';
 
-import 'package:flutter/material.dart';
-import 'package:qr_flutter/qr_flutter.dart';
-
-import '../services/qr_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:geolocator/geolocator.dart';
 
 class GenerateQRButton extends StatefulWidget {
   final bool isLiveWalk;
