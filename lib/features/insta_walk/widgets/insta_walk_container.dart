1b3d7e202ea4fa7dc244d46c4c48243d3f4cb68b
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-22 23:09:49 +0530

    Update insta_walk_container.dart

--- lib/features/insta_walk/widgets/insta_walk_container.dart
+++ lib/features/insta_walk/widgets/insta_walk_container.dart
@@ -11,6 +11,7 @@ import '../services/insta_walk_search_service.dart';
 import 'insta_walk_map_radar.dart';
 import 'insta_walk_retry.dart';
 import 'insta_walk_search_button.dart';
+import 'insta_walk_stop_button.dart';
 import 'insta_walk_searching.dart';
 import '../../../screens/address_screen.dart';
 
